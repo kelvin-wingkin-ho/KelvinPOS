114b07d04787df4a2686132a8bb24d9892a5614e
Kelvin Ho <kelvin.wingkin.ho@gmail.com>
2025-12-06 08:18:12 +1100

    Update

--- Backend/src/services/userService.ts
+++ Backend/src/services/userService.ts
@@ -1,6 +1,12 @@
 import { User } from '@pos/common';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const getAllUsers = async (): Promise<User[]> => {
+    const users  = await prisma.user.findMany();
+    console.log(users);
+
     return [
         {id: 1, name: "Alice"},
         {id: 2, name: "Bob"}
